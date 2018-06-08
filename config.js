f974aa1f35bcb053ea4c4e169f0af15fee9e3d30
webbotio <37455643+webbotio@users.noreply.github.com>
2018-06-08 16:55:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var Config = {
   // Contents of this file will be send to the client
-  "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
+  "domain":     process.env.OPENSHIFT_APP_DNS || 'http://socketio-openshift-socketio-openshift.7e14.starter-us-west-2.openshiftapps.com/',
 
-  "serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  "serverip":   process.env.OPENSHIFT_NODEJS_IP || 'http://socketio-openshift-socketio-openshift.7e14.starter-us-west-2.openshiftapps.com/',
   "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
   
   "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080',
